32aa0f32b0a69e61b03b4c0226a600d79a92d55d
Hanmai001 <giahanmai3110@gmail.com>
2022-12-06 22:48:36 +0700

    setup db

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,5 +1,6 @@
 // get the client
 import mysql from 'mysql2/promise';
+require('dotenv').config();
 
 console.log("Creating connection pool...");
 // create the connection to database
@@ -8,10 +9,10 @@ const db = { connection: null };
 (async () => {
     // create the connection to database
     db.connection = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'onlineshop',
-        password: 'root'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD
     });
     console.log('Database connected!');
 })();
