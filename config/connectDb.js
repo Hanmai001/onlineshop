f765dd98fb3e8d62fc90c0082aefcf323df72f70
Hanmai001 <giahanmai3110@gmail.com>
2022-12-07 00:01:16 +0700

    setup db

--- config/connectDb.js
+++ config/connectDb.js
@@ -3,17 +3,23 @@ import mysql from 'mysql2/promise';
 
 console.log("Creating connection pool...");
 // create the connection to database
-const db = { connection: null };
+// const db = { connection: null };
 
-(async () => {
-    // create the connection to database
-    db.connection = await mysql.createConnection({
+// (async () => {
+//     // create the connection to database
+//     db.connection = await mysql.createConnection({
+//         host: process.env.DB_HOST || 'localhost',
+//         user: process.env.DB_USER || 'root',
+//         database: process.env.DB_NAME || 'onlineshop',
+//         password: process.env.DB_PASSWORD || 'root'
+//     });
+//     console.log('Database connected!');
+// })();
+const db = mysql.createConnection({
         host: process.env.DB_HOST || 'localhost',
         user: process.env.DB_USER || 'root',
         database: process.env.DB_NAME || 'onlineshop',
         password: process.env.DB_PASSWORD || 'root'
-    });
-    console.log('Database connected!');
-})();
+});
 
 export default db;
--- model/productService.js
+++ model/productService.js
@@ -1,9 +1,24 @@
-import db from '../config/connectDB';
+//import db from '../config/connectDB';
+//const db = require('../config/connectDb');
+// get the client
+import mysql from 'mysql2/promise';
 
+const db = { connection: null };
 
+(async () => {
+    // create the connection to database
+    db.connection = await mysql.createConnection({
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        database: process.env.DB_NAME || 'onlineshop',
+        password: process.env.DB_PASSWORD || 'root'
+    });
+    console.log('Database connected!');
+})();
 let getAllProduct = async () => {
     const result = await db.connection.execute('SELECT pd.*, pt.LINK FROM product pd JOIN photo pt on pd.IDPRODUCT = pt.IDPRODUCT GROUP BY pd.IDPRODUCT HAVING COUNT(*) >= 1');
     return result[0];
+
 };
 let getFilterProduct = async (queryFilter) => {
     const {
